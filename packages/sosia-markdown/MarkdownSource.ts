afeb2f34dee93eea0af47d08d071f2852d65cb39
Scott Doxey <neogeek0101@gmail.com>
2019-05-07 12:09:00 -0400

    Replace any non-alpha numeric characters to prevent path issues.

--- packages/sosia-markdown/MarkdownSource.ts
+++ packages/sosia-markdown/MarkdownSource.ts
@@ -13,7 +13,7 @@ export default class MarkdownSource {
         switch (node.type) {
           case 'title':
             const block = node.block[0] as any;
-            title = block && block.text;
+            title = block && block.text.replace(/[^\w]+/gu, '-');
             break;
           case 'codeBlock':
             ex.push({
