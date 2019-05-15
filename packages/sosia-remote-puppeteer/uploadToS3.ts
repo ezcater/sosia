e295264edb547732464db77a203459f01151bb97
Scott Doxey <neogeek0101@gmail.com>
2019-05-15 13:48:08 -0400

    Return remote file path.
    Auto generate filename.

--- packages/sosia-remote-puppeteer/uploadToS3.ts
+++ packages/sosia-remote-puppeteer/uploadToS3.ts
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+import crypto from 'crypto';
+
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({
@@ -8,18 +10,30 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 });
 
-const sendToS3 = (fileName: string, fileContents: string) => {
+const sendToS3 = (fileContents: string, contentType: string, fileExtension: string) => {
   if (!process.env.S3_BUCKET_NAME) {
     throw new Error('Missing AWS bucket name.');
   }
-  s3.putObject(
-    {
+  const bucket = process.env.S3_BUCKET_NAME;
+  const fileName = `${crypto
+    .createHmac('sha256', process.env.CRYPTO_HASH || 'secret')
+    .update(fileContents)
+    .digest('hex')}.${fileExtension}}`;
+  return s3
+    .putObject({
+      ACL: 'public-read',
       Body: fileContents,
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: bucket,
+      ContentType: contentType,
       Key: fileName,
-    },
-    (err: any, data: any) => console.log(err, data)
-  );
+    })
+    .promise()
+    .then(() =>
+      s3.getSignedUrl('getObject', {
+        Bucket: bucket,
+        Key: fileName,
+      })
+    );
 };
 
 export default sendToS3;
